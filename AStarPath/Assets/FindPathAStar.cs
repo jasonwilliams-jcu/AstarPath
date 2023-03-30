ebdf3a7032140f6c8ea8b16878f14a062451ca3d
jasonwilliams-jcu <jason.williams1@my.jcu.edu.au>
2023-03-30 15:35:03 +1000

    generate maze and start and end points
    part 3

--- AStarPath/Assets/FindPathAStar.cs
+++ AStarPath/Assets/FindPathAStar.cs
@@ -35,11 +35,58 @@ public class PathMarker
     public override int GetHashCode()
     {
         return 0;
-
     }
+ }
 
     public class FindPathAStar : MonoBehaviour
     {
+        public Maze maze;
+        public Material closeMaterial;
+        public Material openMaterial;
+
+        List<PathMarker> open = new List<PathMarker>();
+        List<PathMarker> closed = new List<PathMarker>();
+
+        public GameObject start;
+        public GameObject end;
+        public GameObject pathP;
+
+        PathMarker goalNode;
+        PathMarker startNode;
+
+        PathMarker lastPos;
+        bool done = false;
+
+        void RemoveAllMarkers()
+    {
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
+        foreach (GameObject m in markers)
+            Destroy(m);
+    }
+
+    void BeginSearch()
+    {
+        done = false;
+        RemoveAllMarkers();
+
+        List<MapLocation> locations = new List<MapLocation> ();
+        for(int z = 1; z < maze.depth - 1; z++)
+            for (int x = 1; x < maze.depth - 1; x++)
+            {
+                if (maze.map[x,z] != 1)
+                    locations.Add(new MapLocation(x, z));
+            }
+        locations.Shuffle();
+
+        Vector3 startLocation = new Vector3(locations[0].x * maze.scale, 0, locations[0].z * maze.scale);
+        startNode = new PathMarker(new MapLocation(locations[0].x, locations[0].z), 0, 0, 0,
+            Instantiate(start, startLocation, Quaternion.identity), null);
+
+        Vector3 goalLocation = new Vector3(locations[1].x * maze.scale, 0, locations[1].z * maze.scale);
+        goalNode = new PathMarker(new MapLocation(locations[1].x, locations[1].z), 0, 0, 0,
+            Instantiate(end, goalLocation, Quaternion.identity), null);
+    }
+
         // Start is called before the first frame update
         void Start()
         {
@@ -49,7 +96,6 @@ public class PathMarker
         // Update is called once per frame
         void Update()
         {
-
+            if(Input.GetKeyDown(KeyCode.P)) BeginSearch();
         }
     }
-}
